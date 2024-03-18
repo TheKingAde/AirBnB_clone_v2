cdef5ffbcc17740ffe440ef514e85228694e352b
Ade <theking-ade@outlook.com>
2024-03-18 09:36:24 +0000

    change the flask object name to app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Simple Flask web application"""
 from flask import Flask
-app = Flask('__name__')
+app = Flask(__name__)
 
 
 @app.route('/airbnb-onepage', strict_slashes=False)
